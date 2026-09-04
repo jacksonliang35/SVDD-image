910f34b879befc20b222dcece4588d5b7b1a3509
Yuchen Liang <jacksonliang35@gmail.com>
2026-09-04 14:51:44 -0400

    batchnorm to groupnorm

--- compressibility_scorer_cvar.py
+++ compressibility_scorer_cvar.py
@@ -10,17 +10,17 @@ class ResidualBlock_CVaR(nn.Module):
         self.conv1 = nn.Conv2d(
             in_channels, out_channels, kernel_size=3, stride=stride, padding=1, bias=False
         )
-        self.bn1 = nn.BatchNorm2d(out_channels)
+        self.bn1 = nn.GroupNorm(num_groups=32, num_channels=out_channels)
         self.relu = nn.ReLU(inplace=True)
         self.conv2 = nn.Conv2d(
             out_channels, out_channels, kernel_size=3, padding=1, bias=False
         )
-        self.bn2 = nn.BatchNorm2d(out_channels)
+        self.bn2 = nn.GroupNorm(num_groups=32, num_channels=out_channels)
         self.shortcut = nn.Sequential()
         if stride != 1 or in_channels != out_channels:
             self.shortcut = nn.Sequential(
                 nn.Conv2d(in_channels, out_channels, kernel_size=1, stride=stride, bias=False),
-                nn.BatchNorm2d(out_channels),
+                nn.GroupNorm(num_groups=32, num_channels=out_channels),
             )
 
     def forward(self, x):
